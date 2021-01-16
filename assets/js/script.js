b231b509f9f54486e44beb1422a7470dc3bb188a
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2021-01-16 15:57:19 +0100

    16b

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,10 +12,8 @@
     pauseTime: 2000,
     pauseOnHover: false,
     manualAdvance: false,
-    prevText:
-      '<svg width="2em" height="2em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 48 48"><path fill="#2f552e" d="M30.9 43l3.1-3.1L18.1 24L34 8.1L30.9 5L12 24z"/></svg>',
-    nextText:
-      '<svg width="2em" height="2em"  preserveAspectRatio="xMidYMid meet" viewBox="0 0 48 48"><path fill="#2f552e" d="M17.1 5L14 8.1L29.9 24L14 39.9l3.1 3.1L36 24z"/></svg>',
+    prevText: "",
+    nextText: "",
     slices: 30,
     boxCols: 10,
     boxRows: 8,
