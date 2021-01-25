663ac2088c78beb1bd17fc44b36d0de33aae83b9
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2021-01-25 22:45:20 +0100

    aos added final again

--- assets/js/script.js
+++ assets/js/script.js
@@ -85,4 +85,14 @@
       return false;
     });
   });
+  // Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      once: false,
+    });
+  }
+  $(window).on("load", function () {
+    aos_init();
+  });
 })(jQuery);
